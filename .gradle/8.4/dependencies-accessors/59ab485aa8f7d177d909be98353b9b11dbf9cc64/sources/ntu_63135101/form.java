69d1eacd46d09bb0eaa3de3fd08ee5b6552c4dd2
phuc091003 <huuphucnguyen73@gmail.com>
2024-03-25 17:07:46 +0700

    update

--- .gradle/8.4/dependencies-accessors/59ab485aa8f7d177d909be98353b9b11dbf9cc64/sources/ntu_63135101/form.java
+++ .gradle/8.4/dependencies-accessors/59ab485aa8f7d177d909be98353b9b11dbf9cc64/sources/ntu_63135101/form.java
@@ -8,33 +8,18 @@ import java.awt.event.ActionListener;
 public class form extends JFrame implements ActionListener{
 	private JTextField meterTextField;
 	private JTextField feetTextField;
+
 	
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					form frame = new form();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
-	/**
-	 * Create the frame.
-	 */
 	public form() {
+		setTitle("Chuyển đổi đơn vị đo");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-
-		setContentPane(contentPane);
+		setResizable(false);
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridBagLayout());
+		GridBagConstraints constraints = new GridBagConstraints();
+		constraints.insets = new Insets(30,30,30,30);
 	}
 
 }
