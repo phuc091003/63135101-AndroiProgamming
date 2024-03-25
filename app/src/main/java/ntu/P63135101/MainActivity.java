56651eadac0588aef5c4effe46ab82ef0495c35b
phuc091003 <huuphucnguyen73@gmail.com>
2024-03-25 21:13:33 +0700

    Update MainActivity.java

--- app/src/main/java/ntu/P63135101/MainActivity.java
+++ app/src/main/java/ntu/P63135101/MainActivity.java
@@ -1,6 +1,9 @@
 package ntu.P63135101;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +11,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import nt21.test.R;
 
+public class MainActivity extends AppCompatActivity {
+    private EditText heighInput;
+    private EditText weighInput;
+    private Button calculateButton;
+    private TextView resultText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        heighInput = findViewById(R.id.heightInput);
+        weighInput = findViewById(R.id.weightInput);
+        calculateButton = findViewById(R.id.calculateButton);
+        resultText = findViewById(R.id.resultText);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
